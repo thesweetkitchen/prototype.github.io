bc7d293b476656a3fa995f079c26a8a5b2a2136e
thesweetkitchen <admin@thesweet.kitchen>
2020-01-20 18:39:06 +0800

    Testing terminal

--- main.js
+++ main.js
@@ -1,217 +1,84 @@
-// Get references to UI elements
-let connectButton = document.getElementById('connect');
-let disconnectButton = document.getElementById('disconnect');
-let terminalContainer = document.getElementById('terminal');
-let sendForm = document.getElementById('send-form');
-let inputField = document.getElementById('input');
+// UI elements.
+const deviceNameLabel = document.getElementById('device-name');
+const connectButton = document.getElementById('connect');
+const disconnectButton = document.getElementById('disconnect');
+const terminalContainer = document.getElementById('terminal');
+const sendForm = document.getElementById('send-form');
+const inputField = document.getElementById('input');
 
-constructor(serviceUuid = 0xFFE0, characteristicUuid = 0xFFE1,
-      receiveSeparator = '\n', sendSeparator = '\n') {
-    // Used private variables.
-    this._receiveBuffer = ''; // Buffer containing not separated data.
-    this._maxCharacteristicValueLength = 20; // Max characteristic value length.
-    this._device = null; // Device object cache.
-    this._characteristic = null; // Characteristic object cache.
+// Helpers.
+const defaultDeviceName = 'Terminal';
+const terminalAutoScrollingLimit = terminalContainer.offsetHeight / 2;
+let isTerminalAutoScrolling = true;
 
-    // Bound functions used to add and remove appropriate event handlers.
-    this._boundHandleDisconnection = this._handleDisconnection.bind(this);
-    this._boundHandleCharacteristicValueChanged =
-        this._handleCharacteristicValueChanged.bind(this);
+const scrollElement = (element) => {
+  const scrollTop = element.scrollHeight - element.offsetHeight;
 
-    // Configure with specified parameters.
-    this.setServiceUuid(serviceUuid);
-    this.setCharacteristicUuid(characteristicUuid);
-    this.setReceiveSeparator(receiveSeparator);
-    this.setSendSeparator(sendSeparator);
+  if (scrollTop > 0) {
+    element.scrollTop = scrollTop;
   }
-  
-// Connect to the device on Connect button click
-connectButton.addEventListener('click', function() {
-  connect();
-});
+};
 
-// Disconnect from the device on Disconnect button click
-disconnectButton.addEventListener('click', function() {
-  disconnect();
-});
-
-// Handle form submit event
-sendForm.addEventListener('submit', function(event) {
-  event.preventDefault(); // Предотвратить отправку формы
-  send(inputField.value); // Отправить содержимое текстового поля
-  inputField.value = '';  // Обнулить текстовое поле
-  inputField.focus();     // Вернуть фокус на текстовое поле
-});
-
-// Selected device object cache
-let deviceCache = null;
-// Characteristic object cache
-let characteristicCache = null;
-// Intermediate buffer for incoming data
-let readBuffer = '';
-
-// Launch Bluetooth device chooser and connect to the selected
-function connect() {
-  return (deviceCache ? Promise.resolve(deviceCache) :
-      requestBluetoothDevice()).
-      then(device => connectDeviceAndCacheCharacteristic(device)).
-      then(characteristic => startNotifications(characteristic)).
-      catch(error => log(error));
-}
-
-function requestBluetoothDevice() {
-  log('Requesting bluetooth device...');
-
-  //return navigator.bluetooth.requestDevice({
-//    filters: [{services: [0xFFE1]}],
-//  }).
-return navigator.bluetooth.requestDevice({
-  filters: [{services: [this._serviceUuid]}],
-}).
-      then(device => {
-        log('"' + device.name + '" bluetooth device selected');
-        deviceCache = device;
-        deviceCache.addEventListener('gattserverdisconnected',
-            handleDisconnection);
-
-        return deviceCache;
-      });
-}
-
-function handleDisconnection(event) {
-  let device = event.target;
-
-  log('"' + device.name +
-      '" bluetooth device disconnected, trying to reconnect...');
-
-  connectDeviceAndCacheCharacteristic(device).
-      then(characteristic => startNotifications(characteristic)).
-      catch(error => log(error));
-}
-
-// Connect to the device specified, get service and characteristic
-function connectDeviceAndCacheCharacteristic(device) {
-  if (device.gatt.connected && characteristicCache) {
-    return Promise.resolve(characteristicCache);
-  }
-
-  log('Connecting to GATT server...');
-
-  return device.gatt.connect().
-      then(server => {
-        log('GATT server connected, getting service...');
-
-        return server.getPrimaryService(0xFFE1);
-      }).
-      then(service => {
-        log('Service found, getting characteristic...');
-
-        return service.getCharacteristic(0x2902);
-      }).
-      then(characteristic => {
-        log('Characteristic found');
-        characteristicCache = characteristic;
-
-        return characteristicCache;
-      });
-}
-
-// Enable the characteristic changes notification
-function startNotifications(characteristic) {
-  log('Starting notifications...');
-
-  return characteristic.startNotifications().
-      then(() => {
-        log('Notifications started');
-        characteristic.addEventListener('characteristicvaluechanged',
-            handleCharacteristicValueChanged);
-      });
-}
-
-
-// Data receiving
-function handleCharacteristicValueChanged(event) {
-  let value = new TextDecoder().decode(event.target.value);
-
-  for (let c of value) {
-    if (c === '\n') {
-      let data = readBuffer.trim();
-      readBuffer = '';
-
-      if (data) {
-        receive(data);
-      }
-    }
-    else {
-      readBuffer += c;
-    }
-  }
-}
-
-// Received data handling
-function receive(data) {
-  log(data, 'in');
-}
-
-// Output to terminal
-function log(data, type = '') {
+const logToTerminal = (message, type = '') => {
   terminalContainer.insertAdjacentHTML('beforeend',
-      '<div' + (type ? ' class="' + type + '"' : '') + '>' + data + '</div>');
-}
+      `<div${type && ` class="${type}"`}>${message}</div>`);
 
-// Disconnect from the connected device
-function disconnect() {
-  if (deviceCache) {
-    log('Disconnecting from "' + deviceCache.name + '" bluetooth device...');
-    deviceCache.removeEventListener('gattserverdisconnected',
-        handleDisconnection);
-
-    if (deviceCache.gatt.connected) {
-      deviceCache.gatt.disconnect();
-      log('"' + deviceCache.name + '" bluetooth device disconnected');
-    }
-    else {
-      log('"' + deviceCache.name +
-          '" bluetooth device is already disconnected');
-    }
+  if (isTerminalAutoScrolling) {
+    scrollElement(terminalContainer);
   }
+};
 
-  if (characteristicCache) {
-    characteristicCache.removeEventListener('characteristicvaluechanged',
-        handleCharacteristicValueChanged);
-    characteristicCache = null;
-  }
+// Obtain configured instance.
+const terminal = new BluetoothTerminal();
 
-  deviceCache = null;
-}
+// Override `receive` method to log incoming data to the terminal.
+terminal.receive = function(data) {
+  logToTerminal(data, 'in');
+};
 
-function send(data) {
-  data = String(data);
+// Override default log method to output messages to the terminal and console.
+terminal._log = function(...messages) {
+  // We can't use `super._log()` here.
+  messages.forEach((message) => {
+    logToTerminal(message);
+    console.log(message); // eslint-disable-line no-console
+  });
+};
 
-  if (!data || !characteristicCache) {
-    return;
-  }
+// Implement own send function to log outcoming data to the terminal.
+const send = (data) => {
+  terminal.send(data).
+      then(() => logToTerminal(data, 'out')).
+      catch((error) => logToTerminal(error));
+};
 
-  data += '\n';
+// Bind event listeners to the UI elements.
+connectButton.addEventListener('click', () => {
+  terminal.connect().
+      then(() => {
+        deviceNameLabel.textContent = terminal.getDeviceName() ?
+            terminal.getDeviceName() : defaultDeviceName;
+      });
+});
 
-  if (data.length > 20) {
-    let chunks = data.match(/(.|[\r\n]){1,20}/g);
+disconnectButton.addEventListener('click', () => {
+  terminal.disconnect();
+  deviceNameLabel.textContent = defaultDeviceName;
+});
 
-    writeToCharacteristic(characteristicCache, chunks[0]);
+sendForm.addEventListener('submit', (event) => {
+  event.preventDefault();
 
-    for (let i = 1; i < chunks.length; i++) {
-      setTimeout(() => {
-        writeToCharacteristic(characteristicCache, chunks[i]);
-      }, i * 100);
-    }
-  }
-  else {
-    writeToCharacteristic(characteristicCache, data);
-  }
+  send(inputField.value);
 
-  log(data, 'out');
-}
+  inputField.value = '';
+  inputField.focus();
+});
 
-function writeToCharacteristic(characteristic, data) {
-  characteristic.writeValue(new TextEncoder().encode(data));
-}
+// Switch terminal auto scrolling if it scrolls out of bottom.
+terminalContainer.addEventListener('scroll', () => {
+  const scrollTopOffset = terminalContainer.scrollHeight -
+      terminalContainer.offsetHeight - terminalAutoScrollingLimit;
+
+  isTerminalAutoScrolling = (scrollTopOffset < terminalContainer.scrollTop);
+});
